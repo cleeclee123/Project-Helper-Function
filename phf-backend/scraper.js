a799235113267943d100ddb5804d6ab8c7c87f7c
Chris Lee <cl114@illinois.edu>
2022-05-30 15:09:46 -0500

    scraper works

--- phf-backend/scraper.js
+++ phf-backend/scraper.js
@@ -1,27 +1,52 @@
-import * as cheerio from 'cheerio';
-import fetch from 'node-fetch';
+const cheerio = require("cheerio");
+const axios = require("axios");
 
-// Function starts here
-async function getFormulaOneDrivers() {
-  try {
-    const response = await fetch("https://www.formula1.com/en/drivers.html");
-    const body = await response.text();
-    const $ = cheerio.load(body);
 
-    $('.listing-items--wrapper > .row > .col-12').map((index, element) => {
-        const rank = $(element).find(".rank").text();
-        const points = $(element).find(".points > .f1-wide--s").text();
-        const team = $(element).find(".listing-item--team").text();
-        const firstName = $(element).find(".listing-item--name span:first").text();
-        const lastName = $(element).find(".listing-item--name span:last").text();
-      
-        console.log(team + ": " + firstName + " " + lastName + " Points: " + points);
+const searchString = "test";
+const encodedString = encodeURI(searchString);
+
+// write header interface
+const AXIOS_OPTIONS = {
+  headers: {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
+  },
+};
+
+function getOrganicResults() {
+  return axios
+    .get(
+      `https://www.google.com/search?q=${encodedString}&hl=en&gl=us`,
+      AXIOS_OPTIONS
+    )
+    .then(function ({ data }) {
+      let $ = cheerio.load(data);
+
+      const links = [];
+      const titles = [];
+      const snippets = [];
+
+      $(".yuRUbf > a").each((i, el) => {
+        links[i] = $(el).attr("href");
+      });
+      $(".yuRUbf > a > h3").each((i, el) => {
+        titles[i] = $(el).text();
+      });
+      $(".IsZvec").each((i, el) => {
+        snippets[i] = $(el).text().trim();
+      });
+
+      const result = [];
+      for (let i = 0; i < links.length; i++) {
+        result[i] = {
+          link: links[i],
+          title: titles[i],
+          snippet: snippets[i],
+        };
+      }
+
+      console.log(result);
     });
-    
-  } catch (error) {
-    console.log(error);
-  }
 }
 
-// Run getFormulaOneDrivers
-getFormulaOneDrivers();
+getOrganicResults();
