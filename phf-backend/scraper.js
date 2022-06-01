1706fd5e295a1ea92c18e1e8034e8c9fedf6caf5
Chris Lee <cl114@illinois.edu>
2022-06-01 12:55:48 -0500

    break up axios calls

--- phf-backend/scraper.js
+++ phf-backend/scraper.js
@@ -1,6 +1,61 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
+const promise = require("promise");
 
+
+// function to get google search results from axios
+async function fetchGoogleSearchData(searchQuery) {
+  // encode search query to represent UTF-8, URLs can only have certain characters from ASCII set
+  const encodedSearch = encodeURI(searchQuery);
+
+  // default english
+  const languageSearch = "&hl=en";
+
+  // default united states
+  const countrySearch = "&gl=us";
+
+  // default 3,4 results
+  // add "see another solution feature"
+  const numberOfResults = "&num=4";
+
+  // write header interface
+  const options = {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
+    },
+  };
+
+  // google search data with axios
+  const googleSearchData = await axios.get(
+    `https://www.google.com/search?q=${encodedSearch} + ${languageSearch} + ${countrySearch} + ${numberOfResults} `,
+    options
+  );
+
+  // new google search data promise
+  const newGoogleSearchDataPromise = await googleSearchData.data;
+  
+  return newGoogleSearchDataPromise;
+}
+
+// scraps the top title and links for search query
+function getGoogleSearchLinks(searchQuery) {
+  // calls fetchGoogleSearchData from axios (promise)
+  const searchData = fetchGoogleSearchData(searchQuery); 
+
+  // calls .then on promise to capture results
+  // start to scrap here 
+  searchData.then(function(data) {
+    console.log(data);
+  })
+}
+
+// scraps the top title and links for search query
+/* function getGoogleSearchLinks(searchQuery) {
+  const googleSearchData = fetchGoogleSearchData(searchQuery).()
+}  */
+
+/* 
 // scraps the top title and links for search query
 function getGoogleSearchLinks(searchQuery) {
   // encode search query to represent UTF-8, URLs can only have certain characters from ASCII set
@@ -53,10 +108,11 @@ function getGoogleSearchLinks(searchQuery) {
       }
 
       console.log(results);
-    });
-
+    }); 
 }
 
 getGoogleSearchLinks("is a palindrome");
 
+ */
 
+getGoogleSearchLinks("test");
