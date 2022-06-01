595bd91487d53115cd08440db613652d66567fed
Chris Lee <cl114@illinois.edu>
2022-05-31 23:51:46 -0500

    updated scraper

--- phf-backend/scraper.js
+++ phf-backend/scraper.js
@@ -1,47 +1,53 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
+// scraps the top title and links for search query
+function getGoogleSearchLinks(searchQuery) {
+  // encode search query to represent UTF-8, URLs can only have certain characters from ASCII set
+  const encodedSearch = encodeURI(searchQuery);
 
-const searchString = "test";
-const encodedString = encodeURI(searchString);
+  // default english
+  const languageSearch = "&hl=en";
 
-// write header interface
-const AXIOS_OPTIONS = {
-  headers: {
-    "User-Agent":
+  // default united states
+  const countrySearch = "&gl=us";
+
+  // default 3,4 results
+  // add "see another solution feature"
+  const numberOfResults = "&num=4";
+
+  // write header interface
+  const options = {
+    headers: {
+      "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36",
-  },
-};
+    },
+  };
 
-function getOrganicResults() {
   return axios
     .get(
-      `https://www.google.com/search?q=${encodedString}&hl=en&gl=us`,
-      AXIOS_OPTIONS
+      `https://www.google.com/search?q=${encodedSearch} + ${languageSearch} + ${countrySearch} + ${numberOfResults} `,
+      options
     )
     .then(function ({ data }) {
       let $ = cheerio.load(data);
 
       const links = [];
       const titles = [];
-      const snippets = [];
 
       $(".yuRUbf > a").each((i, el) => {
         links[i] = $(el).attr("href");
       });
+
       $(".yuRUbf > a > h3").each((i, el) => {
         titles[i] = $(el).text();
       });
-      $(".IsZvec").each((i, el) => {
-        snippets[i] = $(el).text().trim();
-      });
 
-      const result = [];
+      const results = [];
       for (let i = 0; i < links.length; i++) {
-        result[i] = {
+        results[i] = {
           link: links[i],
           title: titles[i],
-          snippet: snippets[i],
         };
       }
 
@@ -49,4 +55,4 @@ function getOrganicResults() {
     });
 }
 
-getOrganicResults();
+getGoogleSearchLinks("is a palindrome");
