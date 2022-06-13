ac831a1b8a3c7d624da3855184a1212c1f06677d
Chris Lee <cl114@illinois.edu>
2022-06-13 15:06:43 -0500

    updated headers

--- phf-backend/google_scraper.js
+++ phf-backend/google_scraper.js
@@ -1,29 +1,6 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-// write request header interface for bing
-const OPTIONS = {
-  headers: {
-    Accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    // "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    Referer: "https://www.google.com",
-    "Sec-Ch-Ua":
-      '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": '"Windows"',
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
-    "X-Amzn-Trace-Id": "Root=1-629e4d2d-69ff09fd3184deac1df68d18",
-  },
-};
-
 async function generateProxy() {
   let ip_addresses = [];
   let port_numbers = [];
@@ -58,6 +35,32 @@ async function generateProxy() {
       
   return proxy;
 }
+
+// write request header interface for bing
+const OPTIONS = {
+  headers: {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      // "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9",
+    Referer: 
+      "https://www.google.com",
+      "Sec-Ch-Ua":
+        '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+      "Sec-Ch-Ua-Mobile": "?0",
+      "Sec-Ch-Ua-Platform": '"Windows"',
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "cross-site",
+      "Sec-Fetch-User": "?1",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+      "X-Amzn-Trace-Id": "Root=1-629e4d2d-69ff09fd3184deac1df68d18",
+    Proxy:
+      generateProxy(),
+  },
+};
 
 // function to get google search results from axios
 async function fetchGoogleSearchData(searchQuery) {
