a5d1ecbdf38ae8e337cc31ca6640cf5bfcf78203
Chris Lee <cl114@illinois.edu>
2022-06-23 22:42:22 -0500

    bing route updated

--- phf-backend/routes/bing_scraper_route.js
+++ phf-backend/routes/bing_scraper_route.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const bingResultObject = require("./scrapers/bing_scraper");
 
 // get route for bing search result object
-router.post("/bingsearch", async function(request, response) {
-  const results = bingResultObject.getResultDataLinks(request.query.searchquery, request.query.searchquery, request.params.linkstate);
-  response.send(results);
+router.post("/bingsearch", async function (request, response) {
+  var searchQuery = request.query.searchquery;
+  var languageChoice = request.query.searchquery;
+  var stateChoice = request.params.linkstate;
+
+  const results = ((await bingResultObject.getResultDataLinks(
+    searchQuery,
+    languageChoice,
+    stateChoice
+  )) || "Search Failed");
+
+  results.then(async function (data) {
+    response.send(data);
+  });
 });
 
 module.exports = router;
