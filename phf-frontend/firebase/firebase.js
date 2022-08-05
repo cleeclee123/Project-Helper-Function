6fa6cac99ba78a717c4b6783d1fa555b60311b04
Chris Lee <cl114@illinois.edu>
2022-08-05 15:01:38 -0500

    hide firebase shit

--- phf-frontend/firebase/firebase.js
+++ phf-frontend/firebase/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import firebaseVariables from "../env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDFrIGWwrx1Oun0oFL-sc3um60GQR-x9XM",
-  authDomain: "project-helper-function.firebaseapp.com",
-  projectId: "project-helper-function",
-  storageBucket: "project-helper-function.appspot.com",
-  messagingSenderId: "530597922940",
-  appId: "1:530597922940:web:c4ddfd833d28a81eb461f9",
-  measurementId: "G-PV9LCYY02W",
+  apiKey: firebaseVariables.firebaseAPIKey,
+  authDomain: firebaseVariables.authDomain,
+  projectId: firebaseVariables.projectId,
+  storageBucket: firebaseVariables.storageBucket,
+  messagingSenderId: firebaseVariables.messagingSenderId,
+  appId: firebaseVariables.appId,
+  measurementId: firebaseVariables.measurementId,
 };
 
 // Initialize Firebase
