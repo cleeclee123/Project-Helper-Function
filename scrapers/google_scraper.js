8395d7378fb4e7fc71ce841f4275bd8a2e3ae9fd
Chris Lee <cl114@illinois.edu>
2022-08-11 18:41:38 -0500

    google scraper refactor

--- scrapers/google_scraper.js
+++ scrapers/google_scraper.js
@@ -63,7 +63,7 @@ async function optionsUtils() {
   return generateProxy().then(async function (proxy) {
     return { userAgent: rotateUserAgent(), proxy: proxy };
   });
-};
+}
 
 // write request header interface for google
 const OPTIONS = async function () {
@@ -78,7 +78,8 @@ const OPTIONS = async function () {
     }
     return (headersOptions = {
       headers: {
-        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         Referer: "https://www.google.com",
         Connection: "keep-alive",
         DNT: "1",
@@ -86,7 +87,8 @@ const OPTIONS = async function () {
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate",
         "Cache-Control": "max-age=0",
-        "Sec-Ch-Ua": '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        "Sec-Ch-Ua":
+          '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
         "Sec-Ch-Ua-Mobile": "?0",
         "Sec-Ch-Ua-Platform": platformUA,
         "Sec-Fetch-Dest": "document",
@@ -94,7 +96,7 @@ const OPTIONS = async function () {
         "Sec-Fetch-Site": "cross-site",
         "Sec-Fetch-User": "?1",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": data.userAgent, 
+        "User-Agent": data.userAgent,
         "X-Amzn-Trace-Id": "Root=1-629e4d2d-69ff09fd3184deac1df68d18",
       },
     });
@@ -116,7 +118,7 @@ const fetchGoogleSearchData = async function (searchQuery) {
   const numberOfResults = "&num=6";
 
   // wrap inside OPTIONS function callback to return request headers
-   return OPTIONS().then(async function (data) {
+  return OPTIONS().then(async function (data) {
     // bing search data with axios
     const googleSearchData = await axios.get(
       `https://www.google.com/search?q=${encodedSearch} + ${languageSearch} + ${countrySearch} + ${numberOfResults} `,
@@ -173,7 +175,6 @@ const buildGoogleResultObject = async function (searchQuery, pLanguage) {
   );
 
   return searchData.then(async function (data) {
-
     // load markup with cheerio
     let $ = cheerio.load(data.promise);
 
@@ -226,14 +227,21 @@ const fetchFirstGoogleResultPage = async function (searchQuery, pLanguage) {
   // makes call to axios to get data from the first link of google result object
   return await resultsGoogle.then(async function (data) {
     await sleep(1000);
-    console.log("google linkstate 1", data[0].link);
+
+    // console.log("google linkstate 1", data.results[0].link);
+
     // link data from array of google result object with axios
-    const linkData = await axios.get(data[0].link, OPTIONS);
+    const linkData = await axios.get(data.results[0].link, OPTIONS);
 
     // new link data promise
     const linkDataPromise = await linkData.data;
 
-    return { source: data[0].link, linkPromise: linkDataPromise };
+    return {
+      source: data.results[0].link,
+      title: data.results[0].title,
+      linkPromise: linkDataPromise,
+      requestHeader: data.requestHeader,
+    };
   });
 };
 
@@ -247,14 +255,21 @@ const fetchSecondGoogleResultPage = async function (searchQuery, pLanguage) {
   // makes call to axios to get data from the second link of google result object
   return await resultsGoogle.then(async function (data) {
     await sleep(1000);
-    console.log("google linkstate 2", data[1].link);
+
+    //console.log("google linkstate 2", data.results[1].link);
+
     // link data from array of result object with axios
-    const linkData = await axios.get(data[1].link, OPTIONS);
+    const linkData = await axios.get(data.results[1].link, OPTIONS);
 
     // new link data promise
     const linkDataPromise = await linkData.data;
 
-    return { source: data[1].link, linkPromise: linkDataPromise };
+    return {
+      source: data.results[1].link,
+      title: data.results[1].title,
+      linkPromise: linkDataPromise,
+      requestHeader: data.requestHeader,
+    };
   });
 };
 
@@ -268,29 +283,92 @@ const fetchThirdGoogleResultPage = async function (searchQuery, pLanguage) {
   // makes call to axios to get data from the third link of google result object
   return await resultsGoogle.then(async function (data) {
     await sleep(1000);
-    console.log("google linkstate 3", data[2].link);
+
+    // console.log("google linkstate 3", data.results[2].link);
+
     // link data from array of google result object with axios
-    const linkData = await axios.get(data[2].link, OPTIONS);
+    const linkData = await axios.get(data.results[2].link, OPTIONS);
 
     // new link data promise
     const linkDataPromise = await linkData.data;
 
-    return { source: data[2].link, linkPromise: linkDataPromise };
+    return {
+      source: data.results[2].link,
+      title: data.results[2].title,
+      linkPromise: linkDataPromise,
+      requestHeader: data.requestHeader,
+    };
+  });
+};
+
+// function to get data from the fourth link in the google result object from axios
+const fetchFourthGoogleResultPage = async function (searchQuery, pLanguage) {
+  // array of result objects, holds the top three results (link, title) from google
+  const resultsGoogle = buildGoogleResultObject(searchQuery, pLanguage);
+
+  await sleep(1000);
+  // data is array of bing result objects
+  // makes call to axios to get data from the third link of google result object
+  return await resultsGoogle.then(async function (data) {
+    await sleep(1000);
+
+    //console.log("google linkstate 4", data.results[3].link);
+
+    // link data from array of google result object with axios
+    const linkData = await axios.get(data.results[3].link, OPTIONS);
+
+    // new link data promise
+    const linkDataPromise = await linkData.data;
+
+    return {
+      source: data.results[3].link,
+      title: data.results[3].title,
+      linkPromise: linkDataPromise,
+      requestHeader: data.requestHeader,
+    };
+  });
+};
+
+// function to get data from the fifth link in the google result object from axios
+const fetchFifthGoogleResultPage = async function (searchQuery, pLanguage) {
+  // array of result objects, holds the top three results (link, title) from google
+  const resultsGoogle = buildGoogleResultObject(searchQuery, pLanguage);
+
+  await sleep(1000);
+  // data is array of bing result objects
+  // makes call to axios to get data from the third link of google result object
+  return await resultsGoogle.then(async function (data) {
+    await sleep(1000);
+
+    //console.log("google linkstate 5", data.results[4].link);
+
+    // link data from array of google result object with axios
+    const linkData = await axios.get(data.results[4].link, OPTIONS);
+
+    // new link data promise
+    const linkDataPromise = await linkData.data;
+
+    return {
+      source: data.results[4].link,
+      title: data.results[4].title,
+      linkPromise: linkDataPromise,
+      requestHeader: data.requestHeader,
+    };
   });
 };
 
 // returns "code" object
 // function to start scraping data from result object links, takes in searchQuery, pLanguage, and state of result link
-// linkState is an int that repersents which link in the result array (1, 2, 3)
+// linkState is an int that repersents which link in the result array (1, 2, 3, 4, 5)
 // linkState will be a dropdown menu/next button on the frontend
 // default will be the code object from google search result object
 const getResultDataLinks = async function (searchQuery, pLanguage, linkState) {
   // captcha page message from response.data
   const CAPTCHA_MESSAGE =
     "Our systems have detected unusual traffic from your computer network";
-
-  const ERROR_MESSAGE = "// An Error has occured, please try again";
-
+  const CAPTCHA_ERROR = "Captached";
+  const LINK_STATE_ERROR = "Link State Error";
+  const ERROR_MESSAGE = "An Error has ocurred, please try again";
   const BAD_SCRAP =
     "// We didn't have anything to scrape, please try again using a different engine";
 
@@ -330,15 +408,19 @@ const getResultDataLinks = async function (searchQuery, pLanguage, linkState) {
           return BAD_SCRAP;
         }
 
-        return { source: data.source, linkState: linkState, code: code };
+        return {
+          source: data.source,
+          title: data.title,
+          linkState: linkState,
+          code: code,
+          requestHeader: data.requestHeader,
+        };
       })
       .catch(async function (error) {
-        /* if (error.response.data.includes(CAPTCHA_MESSAGE)) {
-        throw new Error("Captcha Error")
-      } */
-        console.log(error);
-        return ERROR_MESSAGE;
-        // throw new Error("Link State 1 Error");
+        if (String(error.response).includes(CAPTCHA_MESSAGE)) {
+          return CAPTCHA_ERROR;
+        }
+        return `${LINK_STATE_ERROR} at ls:${linkState}`;
       });
   } else if (linkState === "2") {
     const googlePageTwo = fetchSecondGoogleResultPage(searchQuery, pLanguage);
@@ -375,15 +457,19 @@ const getResultDataLinks = async function (searchQuery, pLanguage, linkState) {
           return BAD_SCRAP;
         }
 
-        return { source: data.source, linkState: linkState, code: code };
+        return {
+          source: data.source,
+          title: data.title,
+          linkState: linkState,
+          code: code,
+          requestHeader: data.requestHeader,
+        };
       })
       .catch(async function (error) {
-        /* if (error.response.data.includes(CAPTCHA_MESSAGE)) {
-        throw new Error("Captcha Error");
-      } */
-        console.log(error);
-        return ERROR_MESSAGE;
-        //throw new Error("Link State 2 Error");
+        if (String(error.response).includes(CAPTCHA_MESSAGE)) {
+          return CAPTCHA_ERROR;
+        }
+        return `${LINK_STATE_ERROR} at ls:${linkState}`;
       });
   } else if (linkState === "3") {
     const googlePageThree = fetchThirdGoogleResultPage(searchQuery, pLanguage);
@@ -427,42 +513,138 @@ const getResultDataLinks = async function (searchQuery, pLanguage, linkState) {
           return BAD_SCRAP;
         }
 
-        return { source: data.source, linkState: linkState, code: code };
+        return {
+          source: data.source,
+          title: data.title,
+          linkState: linkState,
+          code: code,
+          requestHeader: data.requestHeader,
+        };
       })
       .catch(async function (error) {
-        /* if (error.repsonse.data.includes(CAPTCHA_MESSAGE)) {
-        throw new Error("Captcha Error")
-      } */
-        console.log(error);
-        return ERROR_MESSAGE;
-        //throw new Error("Link State 3 Error");
+        if (String(error.response).includes(CAPTCHA_MESSAGE)) {
+          return CAPTCHA_ERROR;
+        }
+        return `${LINK_STATE_ERROR} at ls:${linkState}`;
+      });
+  } else if (linkState === "4") {
+    const googlePageFour = fetchFourthGoogleResultPage(searchQuery, pLanguage);
+
+    return googlePageFour
+      .then(async function (data) {
+        // load markup with cheerio
+        let $ = cheerio.load(data.linkPromise);
+
+        // build "code" object
+        let code = [];
+
+        // loop through code tag on page
+        $("code").each((index, element) => {
+          code[index] = $(element).text();
+        });
+
+        // if code array is empty, loop through all tags with class "code"
+        if (code.length === 0 || code === undefined) {
+          $(".code").each((index, element) => {
+            code[index] = $(element).text();
+          });
+        }
+
+        // if code array is empty, loop through pre tag on page
+        if (code.length === 0 || code === undefined) {
+          $("pre").each((index, element) => {
+            code[index] = $(element).text();
+          });
+        }
+
+        // if code array is empty, loop through td tag on page
+        if (code.length === 0 || code === undefined) {
+          $("td").each((index, element) => {
+            code[index] = $(element).text();
+          });
+        }
+
+        // if code is empty
+        if (code.length === 0 || code == undefined) {
+          return BAD_SCRAP;
+        }
+
+        return {
+          source: data.source,
+          title: data.title,
+          linkState: linkState,
+          code: code,
+          requestHeader: data.requestHeader,
+        };
+      })
+      .catch(async function (error) {
+        if (String(error.response).includes(CAPTCHA_MESSAGE)) {
+          return CAPTCHA_ERROR;
+        }
+        return `${LINK_STATE_ERROR} at ls:${linkState}`;
+      });
+  } else if (linkState === "5") {
+    const googlePageFive = fetchFifthGoogleResultPage(searchQuery, pLanguage);
+
+    return googlePageFive
+      .then(async function (data) {
+        // load markup with cheerio
+        let $ = cheerio.load(data.linkPromise);
+
+        // build "code" object
+        let code = [];
+
+        // loop through code tag on page
+        $("code").each((index, element) => {
+          code[index] = $(element).text();
+        });
+
+        // if code array is empty, loop through all tags with class "code"
+        if (code.length === 0 || code === undefined) {
+          $(".code").each((index, element) => {
+            code[index] = $(element).text();
+          });
+        }
+
+        // if code array is empty, loop through pre tag on page
+        if (code.length === 0 || code === undefined) {
+          $("pre").each((index, element) => {
+            code[index] = $(element).text();
+          });
+        }
+
+        // if code array is empty, loop through td tag on page
+        if (code.length === 0 || code === undefined) {
+          $("td").each((index, element) => {
+            code[index] = $(element).text();
+          });
+        }
+
+        // if code is empty
+        if (code.length === 0 || code == undefined) {
+          return BAD_SCRAP;
+        }
+
+        return {
+          source: data.source,
+          title: data.title,
+          linkState: linkState,
+          code: code,
+          requestHeader: data.requestHeader,
+        };
+      })
+      .catch(async function (error) {
+        if (String(error.response).includes(CAPTCHA_MESSAGE)) {
+          return CAPTCHA_ERROR;
+        }
+        return `${LINK_STATE_ERROR} at ls:${linkState}`;
       });
   } else {
     return ERROR_MESSAGE;
-    // throw new Error("Link State Error");
   }
 };
 
 module.exports = {
   getResultDataLinks,
-  fetchGoogleSearchData,
   buildGoogleResultObject,
 };
-
-// simple testing
-const code = buildGoogleResultObject("hello world", "cpp");
-code.then(async function (data) {
-  await sleep(1000);
-  console.log(data);
-});
-
-/* const test = buildGoogleResultObject("hello world", "c++");
-test.then(async function (data) {
-  console.log(data);
-}); */
-
-/* const proxy = generateProxy();
-proxy.then(async function(data) {
-  console.log(data);
-})
-*/
